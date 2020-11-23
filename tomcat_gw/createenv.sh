1f53545792d16f88f47b62ffdbaa988c00b6d1e8
arribajim <arribajim@gmail.com>
2020-11-23 17:06:14 -0600

    mv dir to tomcat10

--- tomcat_gw/createenv.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-rm setenv.sh
-servermode=#batch,workqueue,scheduler,messaging,startable,ui
-#servermode=#workqueue
-ccenv=pod
-echo export CATALINA_OPTS=\"\$CATALINA_OPTS -Xms1024m -Xmx1024m -Djava.net.preferIPv4Stack=true\" >> setenv.sh
-#export JAVA_OPTS=\"\$JAVA_OPTS -Dgw.cc.serverid=`uuidgen`${servermode} -Dgw.cc.env=${ccenv}\" >> setenv.sh
-echo export JAVA_OPTS=\"\$JAVA_OPTS -Dgw.cc.env=${ccenv}\" >> setenv.sh
-chmod +x setenv.sh
